ccfa16a90eb06d2e590e460d14de18ccef514c3c
sky <skybride@hotmail.com>
2018-06-09 10:56:47 -0400

    LoadNewLevel

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,99 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour {
+
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rcsThrust = 100f;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //todo somewhere stop sound on death
+        if (state == State.Alive)
+        {
+            Rotate();
+            Thrust();
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(state != State.Alive) { return;} //this way only 1 collision happens
+
+        switch (collision.gameObject.tag)
+        {
+            case "friendly":
+                //do nothing
+                print("friendly");
+                break;
+            case "Finish":
+                state = State.Transcending; //todo
+                Invoke("LoadNextLevel", 1f); //parameterise time
+                break;
+            default:
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f);
+                break;
+        }
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+           
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //resume physics control of rotation
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying) //no layering = no weird sound
+                audioSource.Play();
+        }
+
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+}
